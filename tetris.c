30264c4c0f7ee98a880dc53d2306049b63e0f63f
Guest <guest-5vvxvm@h25977.sgb.skir.kr>
2021-11-01 23:49:35 +0900

    ㅅㅀ어

--- tetris.c
+++ tetris.c
@@ -11,19 +11,21 @@
 typedef struct {
     int (*xy_arr)[2];
     int shape[4][4];
-    void (*spin)(int **);
+    void (*spin)(int (*)[4]);
 } block_struct;
 
 void gotoxy(int x, int y);
 int getch();
 void print_block(block_struct, int, int);
 void print_frame(int (*)[12]);
-void f1(int ** shape);
+void f1(int (*shape)[4]);
 void fill_xy_arr(int (*)[2], int (*)[4]);
 int find_xmax(int (*)[2]);
 int find_xmin(int (*)[2]);
 int find_ymax(int (*)[2]);
-int isanyblock(int (*frame)[12], int * ,int *);
+int isanyblock(int (*frame)[12], int * ,int *, int);
+void oneline_complete(int (*frame)[12]);
+void mv_line(int (*frame)[12], int i);
 
 int size = 0;
 
@@ -102,7 +104,7 @@ int main()
          {0, 1, 1, 1},
          {0, 0, 1, 0}
         },
-        f1}
+        NULL}
     };
 
     printf("\e[?25l"); //커서 깜빡임 제거
@@ -115,14 +117,15 @@ int main()
     {
         if(current == 5)
         {
-            size = 10 * 4;
+            size = (10 * 4)/sizeof(block[current].xy_arr[0]);
         }
         else
         {
-            size = 8 * 4;
+            size = (8 * 4)/sizeof(block[current].xy_arr[0]);
         }
 
         printf("\033[2J");//tput clear
+        oneline_complete(frame);
         gotoxy(1,1);
         print_frame(frame);//게임틀 출력
         gotoxy(x,y);
@@ -136,8 +139,30 @@ int main()
         //printf("■■■■■■■■■■■\n");
        
         key = getch();
-        if(key == 10)
+        if(key == 10)       //LF 방지
             key = getch();
+        if(key == 116)
+        {
+            block[current].spin(block[current].shape);
+            fill_xy_arr(block[current].xy_arr, block[current].shape);
+            for(int i = 0; i < size; i++)
+            {
+                if(x + block[current].xy_arr[i][0] == 1)
+                {
+                    x++;
+                    i = -1;
+                }
+                if(x + block[current].xy_arr[i][0] == 12)
+                {
+                    x--;
+                    i = -1;
+                }
+
+
+                //y가 기존에 frame과 겹치는 경우도 계산
+            }
+            continue;
+        }
         if(key == 27)
         {
             key = getch();
@@ -149,17 +174,16 @@ int main()
                     //아래로 떨구기
                     y = 20 - (ymax + 1);
                     //x + xy_arr[0][0], y + xy_arr[0][1] : frame index
-                    for(int i = 0; i < size/sizeof(block[current].xy_arr[0]); i++)
+                    for(int i = 0; i < size; i++)
                     {
-                        int framex[size/sizeof(block[current].xy_arr[0])], framey[size/sizeof(block[current].xy_arr[0])];
-                        for(int j = 0; j < size/sizeof(block[current].xy_arr[0]); j++)
+                        int framex[size], framey[size];
+                        for(int j = 0; j < size; j++)
                         {
                             framex[j] = x + block[current].xy_arr[j][0] - 1;
                             framey[j] = y + block[current].xy_arr[j][1] - 1;
                         }
-                        printf("%ld", sizeof(framex)/sizeof(framex[0]));
                         //if(해당하는 x좌표에 해당하는 y좌표 위로 하나의 행이라도 블럭이 있으면 return 1;)
-                        if(isanyblock(frame, framex, framey))
+                        if(isanyblock(frame, framex, framey, size))
                         {
                             y--;
                             i = -1;
@@ -172,9 +196,9 @@ int main()
                         }
                         */
                         //frame배열에 겹치는 부분이 없다면 frame에 2값 대입
-                        if(i == size/sizeof(block[current].xy_arr[0]) - 1)
+                        if(i == size - 1)
                         {
-                            for(int j = 0; j < size/sizeof(block[current].xy_arr[0]); j++)
+                            for(int j = 0; j < size; j++)
                             {
                                 frame[framey[j]][framex[j]] = 2;
                             }
@@ -238,7 +262,7 @@ int main()
     
 }
 
-void f1(int ** shape)
+void f1(int (*shape)[4])
 {
     int cp_shape[4][4];
     for(int i = 0; i < 4; i++)
@@ -259,7 +283,7 @@ void f1(int ** shape)
 
 void fill_xy_arr(int (*xy_arr)[2], int (*shape)[4])
 {
-    for(int i = 0; i < size/sizeof(xy_arr[0]); )
+    for(int i = 0; i < size; )
     {
         for(int j = 0; j < 4; j++)
         {
@@ -290,7 +314,7 @@ void gotoxy(int x, int y)
 int find_xmax(int (*xy_arr)[2])
 {
     int xmax = xy_arr[0][0];
-    for(int i = 0; i < size/sizeof(xy_arr[0]); i++)
+    for(int i = 0; i < size; i++)
     {
         if(xmax < xy_arr[i][0])
             xmax = xy_arr[i][0];
@@ -301,7 +325,7 @@ int find_xmax(int (*xy_arr)[2])
 int find_xmin(int (*xy_arr)[2])
 {
     int xmin = xy_arr[0][0];
-    for(int i = 0; i < size/sizeof(xy_arr[0]); i++)
+    for(int i = 0; i < size; i++)
     {
         if(xmin > xy_arr[i][0])
             xmin = xy_arr[i][0];
@@ -312,7 +336,7 @@ int find_xmin(int (*xy_arr)[2])
 int find_ymax(int (*xy_arr)[2])
 {
     int ymax = xy_arr[0][1];
-    for(int i = 0; i < size/sizeof(xy_arr[0]); i++)
+    for(int i = 0; i < size; i++)
     {
         if(ymax < xy_arr[i][1])
             ymax = xy_arr[i][1];
@@ -320,9 +344,9 @@ int find_ymax(int (*xy_arr)[2])
     return ymax;
 }
 
-int isanyblock(int (*frame)[12], int * framex, int * framey)
+int isanyblock(int (*frame)[12], int * framex, int * framey, int size)
 {
-    for(int i = 0; i < sizeof(framex)/sizeof(framex[0]); i++)
+    for(int i = 0; i < size; i++)
     {
         for(int j = framey[i]; j >= 0; j--)
         {
@@ -375,6 +399,36 @@ void print_block(block_struct block, int x, int y)
         //좌측으로 네 칸, 아래로 한 칸 커서 이동
         //printf("\033[u");//저장된 커서 위치로 이동
         //printf("\033[%dB", 1);//아래로 한 칸씩 커서 이동
+    }
+}
+
+void oneline_complete(int (*frame)[12])
+{
+    for(int i = 19; i >= 0; i--)
+    {
+        int cnt = 0;
+        for(int j = 1; j < 11; j++)
+        {
+            if(frame[i][j] == 2)
+                cnt++;
+        }
+        if(cnt == 10)
+        {
+            mv_line(frame, i);
+            i = 20;
+            continue;
+        }
+    }
+}
+
+void mv_line(int (*frame)[12], int i)
+{
+    for( ; i > 0; i--)
+    {
+        for(int j = 1; j < 11; j++)
+        {
+            frame[i][j] = frame[i - 1][j];
+        }
     }
 }
 
